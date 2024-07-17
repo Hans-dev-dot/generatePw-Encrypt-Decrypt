4c5816840ca2d563c981f9b998fd8ee955d3fd5c
Rifki <cryptochess24@gmail.com>
2024-07-18 01:40:33 +0700

    test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js
 
-const apiUrl = 'https://rener-hnj771p84-hans-dev-dots-projects.vercel.app/'; // Ganti dengan URL serverless function Anda
+const apiUrl = 'https://rener-69u3qwhsa-hans-dev-dots-projects.vercel.app'; // Ganti dengan URL serverless function Anda
 
 const passwordForm = document.getElementById('passwordForm');
 const passwordLengthInput = document.getElementById('passwordLength');
